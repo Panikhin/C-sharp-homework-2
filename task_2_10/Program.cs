a1b4dbb6ca5ad59c087676fc4e5b87f9aa18ead7
Дмитрий Панихин <dpanikhin@gmail.com>
2023-06-11 18:40:08 +0300

    fixed task_2_10

--- task_2_10/Program.cs
+++ task_2_10/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
 Console.Clear();
 Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-if ((number > 99) && number < 1000)
+if (number > 99 && number < 1000)
 {
     int result = number % 100 / 10;
     Console.WriteLine($"Вторая цифра трехзначного числа = {result}");
