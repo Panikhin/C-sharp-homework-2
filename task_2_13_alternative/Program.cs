f319f0e9c32b781435cc262376cb3609160b4abd
Дмитрий Панихин <dpanikhin@gmail.com>
2023-06-13 22:34:01 +0300

    task_2_13 alternative done

--- task_2_13_alternative/Program.cs
+++ task_2_13_alternative/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (number < 100)
+{
+    Console.WriteLine("Ошибка! В числе третьей цифры нет!");
+}
+else
+{
+    if (number < 1000)
+    {
+        Console.WriteLine(number % 10);
+    }
+    else
+    {
+        while (number > 999)
+        {
+        number = number / 10;
+        }
+        Console.WriteLine(number % 10);
+    }
+}
